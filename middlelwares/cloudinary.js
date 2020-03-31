c2bdbc88f9b199730c962525f06389f1f8232602
amir ghedira <amirghedira06@gmail.com>
2020-03-31 06:36:17 +0100

    V1.0

--- middlelwares/cloudinary.js
+++ middlelwares/cloudinary.js
@@ -6,9 +6,9 @@ const uploader = cloudinary.uploader
 dotenv.config();
 const cloudinaryConfig =
     cloudinary.config({
-        cloud_name: "hoz1jpmp7",
-        api_key: "287757447646335",
-        api_secret: "1jgYtcyaC2soXm0yNOw7gw3lFKU"
+        cloud_name: "cloud_name",
+        api_key: "cloud_key",
+        api_secret: "cloud_secret"
     });
 exports.cloudinaryConfig = cloudinaryConfig;
 exports.uploader = uploader;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = express();
 const path = require('path');
 const projectRoutes = require('./routes/project');
 const userRoutes = require('./routes/user')
-mongosse.connect("mongodb+srv://amirghedirq:RVaeoXZ1Lrk1U9s2@cluster0-bjmuu.mongodb.net/test?retryWrites=true&w=majority", {
+mongosse.connect("mongoinfo", {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
