61812e35007f4317e269939d8a38f125ef389d7c
amir ghedira <amirghedira06@gmail.com>
2020-04-01 06:56:48 +0100

    V1.0

--- middlelwares/cloudinary.js
+++ middlelwares/cloudinary.js
@@ -6,9 +6,9 @@ const uploader = cloudinary.uploader
 dotenv.config();
 const cloudinaryConfig =
     cloudinary.config({
-        cloud_name: "hojbdhnz4",
-        api_key: "715639388368954",
-        api_secret: "ozpx4Mamw0hXO28kikNfYnqOXMs"
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET
     });
 exports.cloudinaryConfig = cloudinaryConfig;
 exports.uploader = uploader;
